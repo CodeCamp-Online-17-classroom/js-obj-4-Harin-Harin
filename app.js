86077d49ecb5a306dcadb010e666da19111310eb
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-14 19:26:14 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let username = prompt('Username')
+let email = prompt('E-mail')
+let password = prompt('Password')
+
+let user = {
+  Username : username,
+  Email : email,
+  Password : password
+};
+console.log(user)
+alert(`Username: ${user.Username}\nEmail: ${user.Email}\nPassword: ${user.Password}`);
